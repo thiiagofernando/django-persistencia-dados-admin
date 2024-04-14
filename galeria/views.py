2010a3e9f128cdae576dead9822e7f2db74f9fc1
Thiago Fernando <thiago.fernando@msn.com>
2024-04-14 13:06:57 -0400

    Parte 1 Lidando com dados

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+    1:{"nome":"Nebulosa de Carina","legenda":"webbtescope.org / NASA/ James WEB"},
+    2:{"nome":"Galaxia NGC 2170","legenda":"nasa.org / NASA/ Hubble"},
+    }
+    return render(request, 'galeria/index.html', {"cards":dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
